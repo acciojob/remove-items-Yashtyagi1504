e974690f973903a3a49d7561eea4c9415d4eebff
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2025-02-08 01:48:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let btn = document.querySelector("#btn");
+let select = document.querySelector("#colorSelect");
+let option = document.querySelectorAll("option");
+btn.addEventListener("click",removeColor);
+
+function removeColor(){
+	// console.log(option)
+	option[select.selectedIndex].remove()
+}
